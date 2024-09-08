2353168910b665509f1ee1cc81ef83bb88eff406
Jaeeun Jung <rlt231022@gmail.com>
2024-09-08 20:26:59 +0900

    장바구니

--- lib/pages/CartPage.dart
+++ lib/pages/CartPage.dart
@@ -12,94 +12,137 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
-  // List to keep track of selected items
   List<bool> selectedItems = [];
 
   @override
   void initState() {
     super.initState();
-    // Initialize the selection state for each item to false
     selectedItems = List<bool>.filled(widget.cartItems.length, false);
+  }
+
+  // Method to calculate the total price of selected items
+  double getTotalPrice() {
+    double total = 0;
+    for (int i = 0; i < widget.cartItems.length; i++) {
+      if (selectedItems[i]) {
+        total += widget.cartItems[i].price;
+      }
+    }
+    return total;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFF404040), // AppBar의 색상을 검은색으로 설정
+        backgroundColor: Color(0xFF404040),
         leading: IconButton(
           icon: Icon(Icons.chevron_left, color: Color(0xFFDDDDDD)),
           onPressed: () {
-            Navigator.pop(context); // 이전 화면으로 돌아가기
+            Navigator.pop(context);
           },
         ),
         title: Row(
           children: <Widget>[
             Icon(
               Icons.shopping_cart,
-              color: Colors.white, // 아이콘 색상
+              color: Colors.white,
             ),
-            SizedBox(width: 8), // 아이콘과 텍스트 사이의 간격 조정
+            SizedBox(width: 8),
             Text(
-              '장바구니', // 타이틀 설정
+              '장바구니',
               style: TextStyle(
                 fontSize: 24,
-                color: Colors.white, // 타이틀 글자 색상
+                color: Colors.white,
               ),
             ),
           ],
         ),
       ),
-      backgroundColor: Color(0xFFF2EBDF),
+      backgroundColor: Color(0xFFF2EBDF), // 화면 배경색
       body: Column(
         children: <Widget>[
           SizedBox(
             height: 10,
             child: Container(
-              color: Colors.orangeAccent, // 상단 구분선 주황색
+              color: Colors.orangeAccent,
             ),
           ),
-          Expanded(
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  // 장바구니에 있는 상품들을 ListView.builder로 표시
-                  ListView.builder(
-                    shrinkWrap: true, // ListView가 Column 안에 들어가므로 shrinkWrap 사용
-                    physics: NeverScrollableScrollPhysics(), // 내부 스크롤 방지
-                    itemCount: widget.cartItems.length, // 장바구니에 있는 상품 개수
-                    itemBuilder: (context, index) {
-                      // cart 리스트에서 각 상품 가져오기
-                      Product product = widget.cartItems[index];
-                      bool isSelected = selectedItems[index]; // Get selection state
-                      return Card(
-                        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-                        color: isSelected ? Colors.blue[100] : Colors.white, // Change color if selected
-                        child: ListTile(
-                          leading: Image.asset(
-                            product.image,
-                            width: 50,
-                            height: 50,
-                          ),
-                          title: Text(
-                            product.title,
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                          ),
-                          subtitle: Text("₩${product.price}"), // 가격 표시
-                          onTap: () {
-                            setState(() {
-                              selectedItems[index] = !selectedItems[index]; // Toggle selection state
-                            });
-                          },
-                        ),
-                      );
-                    },
-                  ),
-                ],
+          Padding(
+            padding: const EdgeInsets.only(left: 16, top: 16, right: 16),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                '구매하실 상품을 클릭해주세요',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
             ),
           ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: widget.cartItems.length,
+              itemBuilder: (context, index) {
+                Product product = widget.cartItems[index];
+                bool isSelected = selectedItems[index];
+                return Card(
+                  margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  color: isSelected ? Colors.blue[100] : Colors.white,
+                  child: ListTile(
+                    leading: Image.asset(
+                      product.image,
+                      width: 50,
+                      height: 50,
+                    ),
+                    title: Text(
+                      product.title,
+                      style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    subtitle: Text("${product.price.toInt()}"), // 소수점 없애기
+                    onTap: () {
+                      setState(() {
+                        selectedItems[index] = !selectedItems[index];
+                      });
+                    },
+                  ),
+                );
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              '총 합계 : ${getTotalPrice().toInt()}', // 소수점 없애기
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0), // 양쪽 여백 추가
+            child: SizedBox(
+              width: double.infinity, // 버튼 너비를 화면 너비에 맞추기
+              height: 50, // 버튼 높이를 50으로 조정
+              child: ElevatedButton(
+                onPressed: () {
+                  // 결제 로직을 여기에 추가하세요
+                },
+                child: Text('결제하기'),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.orangeAccent, // 버튼 배경색
+                  foregroundColor: Colors.white, // 버튼 텍스트 색상
+                  textStyle: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0), // 버튼의 모서리를 둥글게
+                  ),
+                  padding: EdgeInsets.zero, // 버튼의 여백 없애기
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
         ],
       ),
     );
